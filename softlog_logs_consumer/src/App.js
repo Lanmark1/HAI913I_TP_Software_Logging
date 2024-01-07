936a364ffcdef39a4405fba5da85f0487a2743fd
Jérémie B <jeremie.bentolila@gmail.com>
2024-01-07 23:17:38 +0100

    Ajout parsing logs locaux dans l'app React

--- softlog_logs_consumer/src/App.js
+++ softlog_logs_consumer/src/App.js
@@ -5,43 +5,47 @@ import React, { useEffect, useState } from 'react';
 import LogComponent from './Components/LogComponent';
 import axios from 'axios';
 
+// console.log(data);
 const App = () => {
     const logs = ["user 1", "user 2"];
-    const [data, setData] = useState([]);
-    const test = [data[0].user.name];
-    const fetchData = () => {
+    const [dbData, setDistantData] = useState([]);
+    const [localData, setLocalData] = useState('');
+    const fetchDistantData = () => {
         axios.get('http://localhost:8080/api/users/data')
             .then(response => {
-                console.log(response);
-                console.log(JSON.stringify(response.data[0].user.name));
-                setData(response.data); // Update fetched data in state
+                setDistantData(response.data); // Update fetched data in state
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
     };
 
-    useEffect(() => {
-        // Initial data fetch when component mounts
-        fetchData();
-    }, []);
+    const fetchLocalData = () => {
+        fetch('./application.log')
+            .then((res) => res.text())
+            .then(text  => {
+                setLocalData(text);
+            })
+    };
 
     return (
         <div className="App">
             <img src={logo} className="App-logo" alt="logo" width="300" height="300"/>
             <h1>Log Display App</h1>
-            {JSON.stringify(data)}
+            {localData}
+            {JSON.stringify(dbData)}
             <div className="card-container">
-                <button onClick={fetchData}>Fetch Data</button>
-                <div className="card">
-                    <LogComponent retrievedUsers={test} userType={'most read operations'}/>
-                </div>
-                <div className="card">
-                    <LogComponent retrievedUsers={logs} userType={'most write operations'}/>
-                </div>
-                <div className="card">
-                    <LogComponent retrievedUsers={logs} userType={'most expensive product searched'}/>
-                </div>
+                <button onClick={fetchLocalData}>Fetch Local Data</button>
+                <button onClick={fetchDistantData}>Fetch Distant Data</button>
+            </div>
+            <div className="card">
+                <LogComponent retrievedUsers={logs} userType={'most read operations'}/>
+            </div>
+            <div className="card">
+                <LogComponent retrievedUsers={logs} userType={'most write operations'}/>
+            </div>
+            <div className="card">
+                <LogComponent retrievedUsers={logs} userType={'most expensive product searched'}/>
             </div>
         </div>
     );
